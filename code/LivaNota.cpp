3ad75eb6c6a95001121394b727449e6fe6496f0f
nikozdev <nikozdev@nikozdev.net>
2023-09-06 12:21:48 -0700

    !code!envi!test!libs!deps!refactor!scale!standard!header!include
    [code/LivaNota.cpp] > testing refactor, added filesystem test
    [code/LivaNota.hpp]
    > included and organize standard headers
    > included fmt library
    > added nFileSystem namespace alias for std::filesystem
    > added interface library
    [envi/CMakeLists.txt]
    > added executable and testing build options
    > added fmt library

--- code/LivaNota.cpp
+++ code/LivaNota.cpp
@@ -3,10 +3,69 @@
 //headers
 #include "LivaNota.hpp"
 //content
+namespace nLivaNota
+{
+//testing
+#if defined(dLivaNota_MakeTest)
+//-//typedef
+using tTestKey = std::string_view;
+using tTestOut = int;
+using tTestFun = std::function<tTestOut(void)>;
+using tTestTab = std::unordered_map<tTestKey, tTestFun>;
+using tTestRef = tTestTab::iterator;
+//-//consdef
+static const tTestTab vTestTab = {
+	{"Hello",
+	 []()
+	 {
+		 fmt::println(stdout, "HelloWorld");
+		 return EXIT_SUCCESS;
+	 }},
+	{"FileSystem",
+	 []()
+	 {
+		 auto vAbsolutePath = nFileSystem::current_path();
+		 auto vRelativePath = nFileSystem::relative(vAbsolutePath);
+		 fmt::println(stdout, "RelativePath={:s}", vRelativePath.c_str());
+		 fmt::println(
+			 stdout, "ProjPathFound={:d}", std::filesystem::exists(dLivaNota_ProjPath)
+		 );
+		 fmt::println(
+			 stdout, "DataPathFound={:d}", std::filesystem::exists(dLivaNota_ProjPath)
+		 );
+		 return EXIT_SUCCESS;
+	 }},
+};
+#endif//ifd(dLivaNota_MakeTest)
+}//namespace nLivaNota
 //actions
 int main(int vArgC, char **vArgV, char **vEnvi)
 {
-  auto vStreamIter = std::ostream_iterator<char *>(std::cout, "\n");
-	std::copy(vArgV, vArgV + vArgC, vStreamIter);
-}//main
+  nLivaNota::nFileSystem::current_path(dLivaNota_ProjPath);
+#if defined(dLivaNota_MakeTest)
+	if(vArgC == 3 && std::string_view(vArgV[1]) == "test")
+	{
+		auto vTestKey = std::string_view(vArgV[2]);
+		auto vTestRef = nLivaNota::vTestTab.find(vTestKey);
+		if(vTestRef == nLivaNota::vTestTab.end())
+		{
+			fmt::println(stderr, "invalid test key: {}", vTestKey);
+			return EXIT_FAILURE;
+		}
+		else
+		{
+			fmt::println(stdout, "{}?", vTestKey);
+			auto vTestOut = vTestRef->second();
+			fmt::println(stdout, "{}!", vTestKey);
+			return vTestOut;
+		}
+	}
+	else
+#endif//ifd(dLivaNota_MakeTest)
+	{
+		auto vIter = std::ostream_iterator<char *>(std::cout, "\n");
+		std::copy(vArgV, vArgV + vArgC, vIter);
+	}
+	return EXIT_SUCCESS;
+}
 #endif//dLivaNota_Cpp
--- code/LivaNota.hpp
+++ code/LivaNota.hpp
@@ -1,9 +1,22 @@
 #ifndef dLivaNota_Hpp
 #define dLivaNota_Hpp
 //headers
+#include <cstdlib>
+#include <functional>
+//-//template library
+#include <array>
+#include <vector>
+#include <unordered_map>
+//-//strings
+#include <string_view>
+#include "fmt/format.h"
+//-//input output
 #include <iostream>
+#include <fstream>
+#include <filesystem>
 //content
 namespace nLivaNota
 {
+namespace nFileSystem = std::filesystem;
 }//namespace nLivaNota
 #endif//dLivaNota_Hpp
